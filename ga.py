5494843f4cf36a18b96dd54cc3fd0cdcfb453bba
jperez <perezxavi@gmail.com>
2019-04-04 16:33:41 +0200

    x

--- ga.py
+++ ga.py
@@ -86,96 +86,39 @@ def mutation(offspring_crossover):
 
 
 
-"""
-The y=target is to maximize this equation ASAP:
-	y = pow(w1x1,2)+pow(w2x2, 2)
-	where (x1,x2)=(1,1)
-	What are the best values for the 2 weights w1 to wn?
-	We are going to use the genetic algorithm for the best possible values after a number of generations.
-"""
+def continuous_genetic_algorithm(equation_inputs, sol_per_pop, num_parents, num_generations
 
-# Inputs of the equation.
-equation_inputs = [1,1,1,1,1,1]
+	# Defining the population size.
+	pop_size = (sol_per_pop,num_weights) 
+	#Creating the initial population.
+	new_population = numpy.random.uniform(low=-1000.0, high=1000.0, size=pop_size)
 
-# Number of the weights we are looking to optimize.
-num_weights = len(equation_inputs)
+	best_outputs = []
 
-"""
-Genetic algorithm parameters:
-	Parents set size
-	Population size
-"""
-sol_per_pop = 100 
-num_parents = 98 #odd number, please
-num_generations = 5000
-# Defining the population size.
-pop_size = (sol_per_pop,num_weights) # The population will have sol_per_pop chromosome where each chromosome has num_weights genes.
-#Creating the initial population.
-new_population = numpy.random.uniform(low=-1000.0, high=1000.0, size=pop_size)
-print(new_population)
-
-"""
-new_population[0, :] = [2.4,  0.7]
-new_population[1, :] = [-0.4, 2.7]
-new_population[2, :] = [-1,   2]
-new_population[3, :] = [4,	7]
-new_population[4, :] = [3.1,  4]
-new_population[5, :] = [-2,   3]
-"""
-
-best_outputs = []
-
-for generation in range(num_generations):
+	for generation in range(num_generations):
 	
-   
+		# Measuring the fitness of each chromosome in the population.
+		fitness = pop_fitness(equation_inputs, new_population)
+
+		best_outputs.append(numpy.min(fitness))
+		# The best result in the current iteration.
+		print("Best result : ", numpy.min(fitness))
 	
-	# Measuring the fitness of each chromosome in the population.
+		elite = select_elite(new_population, fitness, sol_per_pop-num_parents)
+
+		# Selecting the best parents in the population.
+		parents = select_parents(new_population, fitness, num_parents)
+	 
+		offspring_crossover = crossover(parents, offspring_size=(num_parents, num_weights))
+		offspring_mutation = mutation(offspring_crossover)
+
+		new_population[0:elite.shape[0], :] = elite
+		new_population[elite.shape[0]:, :] = offspring_mutation
+	
+
 	fitness = pop_fitness(equation_inputs, new_population)
-	#print("Fitness")
-	#print(fitness)
-	
+	best_match_idx = numpy.where(fitness == numpy.min(fitness))
 
-	best_outputs.append(numpy.min(fitness))
-	# The best result in the current iteration.
-	print("Best result : ", numpy.min(fitness))
-	
-	elite = select_elite(new_population, fitness, sol_per_pop-num_parents)
-
-	# Selecting the best parents in the population.
-	parents = select_parents(new_population, fitness, num_parents)
+	return fitness, new_population[best_match_idx, :]
 
 
-	#print("Parents")
-	#print(parents)
-
-
- 
-	# Generating next generation using crossover.
-	offspring_crossover = crossover(parents, offspring_size=(num_parents, num_weights))
-	#print("Crossover")
-	#print(offspring_crossover)
-
-	# Adding some variations to the offspring using mutation.
-	offspring_mutation = mutation(offspring_crossover)
-	#print("Mutation")
-	#print(offspring_mutation)
-
-	# Creating the new population based on the parents and offspring.
-	new_population[0:elite.shape[0], :] = elite
-	new_population[elite.shape[0]:, :] = offspring_mutation
-	
-# Getting the best solution after iterating finishing all generations.
-#At first, the fitness is calculated for each solution in the final generation.
-fitness = pop_fitness(equation_inputs, new_population)
-# Then return the index of that solution corresponding to the best fitness.
-best_match_idx = numpy.where(fitness == numpy.min(fitness))
-
-print("Best solution : ", new_population[best_match_idx, :])
-print("Best solution fitness : ", fitness[best_match_idx])
-
-
-
-matplotlib.pyplot.plot(best_outputs[1000:])
-matplotlib.pyplot.xlabel("Iteration")
-matplotlib.pyplot.ylabel("Fitness")
-matplotlib.pyplot.show()
